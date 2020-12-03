fe3ba99d3af15f780e448595f013e89ad1bc79c0
Omran Alhaddad <44875477+Omrna@users.noreply.github.com>
2020-12-03 16:39:36 +0000

    Create test.c

---
+++ test.c
@@ -0,0 +1,17 @@
+/* vul_prog.c */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define SECRET1 0x44
+#define SECRET2 0x55
+
+void processString(const char *input)
+{
+    char *buffer = malloc(strlen(input));
+}
+
+int main(int argc, char *argv[])
+{
+    return 0;
+}
